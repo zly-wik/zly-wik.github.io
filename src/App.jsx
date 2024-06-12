52a106bfbcdaa2d0128362ec2f6f0faa2da25001
Zly Wik <wiktor.dlugi.bader@gmail.com>
2024-06-12 11:18:57 +0200

    Updated Animator component, removed Sticky() as it make some buttons unclickable

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,38 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
-import { ScrollContainer, ScrollPage, Animator, Fade, batch, Sticky, MoveIn } from 'react-scroll-motion';
+import { ScrollContainer, ScrollPage, Animator, Fade, batch, MoveIn } from 'react-scroll-motion';
 
 function App() {
   return (
     <ScrollContainer snap='mandatory' className='scroll-container'>
 
       <ScrollPage className='scroll-page'>
-        <Animator animation={batch(Fade(), MoveIn(0, 100), Sticky() )} className='scroll-animator'>
+        <Animator animation={batch(Fade(), MoveIn(0, 100) )} className='scroll-animator'>
           <Home />
         </Animator>
       </ScrollPage>
 
       <ScrollPage className='scroll-page'>
-        <Animator animation={batch(Fade(), MoveIn(0, 100), Sticky())} className='scroll-animator'>
+        <Animator animation={batch(Fade(), MoveIn(0, 100) )} className='scroll-animator'>
           <About />
         </Animator>
       </ScrollPage>
 
       <ScrollPage className='scroll-page'>
-        <Animator animation={batch(Fade(), MoveIn(0, 100), Sticky())} className='scroll-animator'>
+        <Animator animation={batch(Fade(), MoveIn(0, 100))} className='scroll-animator'>
           <Projects />
         </Animator>
       </ScrollPage>
 
       <ScrollPage className='scroll-page'>
-        <Animator animation={batch(Fade(), MoveIn(0, 100), Sticky())} className='scroll-animator'>
+        <Animator animation={batch(Fade(), MoveIn(0, 100))} className='scroll-animator'>
           <Skills />
         </Animator>
       </ScrollPage>
 
       <ScrollPage className='scroll-page'>
-        <Animator animation={batch(Fade(), MoveIn(0, 100), Sticky())} className='scroll-animator'>
+        <Animator animation={batch(Fade(), MoveIn(0, 100))} className='scroll-animator'>
           <Contact />
         </Animator>
       </ScrollPage>
