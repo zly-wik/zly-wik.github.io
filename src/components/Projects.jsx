ec7a8add6859d24af2617e0ff4cd4a2b8e930da7
Zly Wik <wiktor.dlugi.bader@gmail.com>
2024-06-12 11:16:55 +0200

    Updated Project page to use buttons like skills page on small screens

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
 export default function Projects() {
-    const [alignment, setAlignment] = React.useState('blog');
+    const [project, setProject] = React.useState('blog');
 
     const handleChange = (newAlignment) => {
         console.log(newAlignment);
         if (newAlignment !== null) {
-            setAlignment(newAlignment);
+            setProject(newAlignment);
         }
     };
 
@@ -16,12 +16,12 @@ export default function Projects() {
             <h1 className='title-header'>My Projects</h1>
         </div>
         <div className='skills-box-group small-screens-only'>
-        <div className='skills-toggle-buttons-group' value={alignment}>
-            <div className={alignment === 'blog' ? 'skills-toggle-button-selected' : 'skills-toggle-button'} onClick={() => handleChange('blog')}>Blog</div>
-            <div className={alignment === 'url-shortener' ? 'skills-toggle-button-selected' : 'skills-toggle-button'} onClick={() => handleChange('url-shortener')}>URL Shortener</div>
-            <div className={alignment === 'shopping-list' ? 'skills-toggle-button-selected' : 'skills-toggle-button'} onClick={() => handleChange('shopping-list')}>Shopping List</div>
+        <div className='skills-toggle-buttons-group' value={project}>
+            <div className={project === 'blog' ? 'skills-toggle-button-selected' : 'skills-toggle-button'} onClick={() => handleChange('blog')}>Blog</div>
+            <div className={project === 'url-shortener' ? 'skills-toggle-button-selected' : 'skills-toggle-button'} onClick={() => handleChange('url-shortener')}>URL Shortener</div>
+            <div className={project === 'shopping-list' ? 'skills-toggle-button-selected' : 'skills-toggle-button'} onClick={() => handleChange('shopping-list')}>Shopping List</div>
         </div>
-        { alignment === 'blog' ? <div className='skills-box'>
+        { project === 'blog' ? <div className='skills-box'>
                 <h2 className='skills-box-header'>
                     Blog
                 </h2>
@@ -31,7 +31,7 @@ export default function Projects() {
                     Blog backend project with integrated comments, discussion panel and few others for my personal blog I plan to start in the next September.
                 </div>
             </div> : null}
-        { alignment === 'url-shortener' ? <div className='skills-box'>
+        { project === 'url-shortener' ? <div className='skills-box'>
                 <h2 className='skills-box-header'>
                     URL Shortener
                 </h2>
@@ -41,7 +41,7 @@ export default function Projects() {
                     Backend application to create shortened versions of long urls to help sharing them with friends.
                 </div>
             </div> : null}
-        { alignment === 'shopping-list' ? <div className='skills-box'>
+        { project === 'shopping-list' ? <div className='skills-box'>
                 <h2 className='skills-box-header'>
                     Shopping List
                 </h2>
